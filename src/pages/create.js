a371261c5521cd13237419d747c94973fd8146be
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-10 12:53:18 +0530

    Added UI for Create page

--- src/pages/create.js
+++ src/pages/create.js
@@ -1,10 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
 
 function Create() {
+    const [title, setTitle] = useState('');
+    const [content, setContent] = useState('');
+
+    const titleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const contentChange = (event) => {
+        setContent(event.target.value);
+    }
 
     return (
         <>
-            Create Page
+            <h2>Create Note</h2>
+            <TextField
+                id="title-input"
+                label="Title"
+                value={title}
+                onChange={titleChange}
+                variant="filled"
+                fullWidth
+            />
+
+            <TextField
+                id="content-input"
+                label="Content"
+                value={content}
+                onChange={contentChange}
+                multiline
+                placeholder="Enter your note content here"
+                variant="filled"
+                fullWidth
+                margin="normal"
+            />
         </>
     );
 }
