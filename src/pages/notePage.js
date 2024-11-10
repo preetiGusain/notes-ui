6c5b30a52c5609cd46bd2f00b4fa2c3d75acfecc
Preeti Gusain <preetigusain@Preetis-MacBook-Air.local>
2024-11-10 13:41:16 +0530

    Added delete button in notePage

--- src/pages/notePage.js
+++ src/pages/notePage.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import { useNavigate } from 'react-router-dom';
 
 function NotePage() {
     const { id } = useParams();
     const [note, setNote] = useState(null);
+    const navigate = useNavigate();
 
     useEffect(() => {
         const getNotes = async () => {
@@ -20,10 +24,29 @@ function NotePage() {
         getNotes();
     }, []);
 
+    const deleteNote = async () => {
+        try {
+            const request = new Request(`https://notes-api-y7g7.onrender.com/delete/${id}`, {
+                method: "DELETE",
+            });
+
+            const response = await fetch(request);
+            if(response.ok) {
+                navigate('/');
+            }
+        } catch (error) {
+            console.error("note not deleted", error);
+        }
+    };
+
+
     return (
         <div>
             <h1>{note?.title}</h1>
             <p>{note?.content}</p>
+            <Button variant="outlined" startIcon={<DeleteIcon />} onClick={() => deleteNote()}>
+                Delete
+            </Button>
         </div>
     );
 }
